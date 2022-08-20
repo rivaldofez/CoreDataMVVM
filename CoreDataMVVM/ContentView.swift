bd7c95fa81cc748409c56b916ec34d8df70b3ed7
rivaldofez <rivaldofez@gmail.com>
2022-08-20 22:55:57 +0700

    Initial Commit

---
+++ CoreDataMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataMVVM
+//
+//  Created by Rivaldo Fernandes on 20/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataMVVM/CoreDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMVVMApp.swift
+//  CoreDataMVVM
+//
+//  Created by Rivaldo Fernandes on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
